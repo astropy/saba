bf022d262687b4b67a92b42e660e47672e3e44c8
Ze Vinicius <jvmirca@gmail.com>
2016-12-02 10:14:20 -0300

    update mcmc docs

--- saba/main.py
+++ saba/main.py
@@ -125,7 +125,7 @@ class SherpaMCMC(object):
                                              pars))
         else:
             raise AstropyUserWarning("Must have valid fit! "
-                                     "Convariance matrix is not present")
+                                     "Covariance matrix is not present")
 
     def __call__(self, niter=200000):
         '''
