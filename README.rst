425c128c7e7f1f1f2674b2d505a7a4d96fd636eb
hamogu <hgunther@mit.edu>
2020-03-02 13:45:57 -0500

    Update to post astropy 4.0, with tox and without helpers

---
+++ README.rst
@@ -0,0 +1,5 @@
+SABA is a code to brigde astropy and and Sherpa modelling and fitting. Both
+packages provide a a modelling and fitting framework that is in principle
+compatible, but uses different APIs. SABA translates between the two packages,
+so that e.g. sherpa models and fitters cna be used inside the astropy modelling
+framework.
--- saba/__init__.py
+++ saba/__init__.py
@@ -7,20 +7,9 @@ Bridge between Sherpa and Astropy modeling.
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *
+from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
-import sys
-
-__minimum_python_version__ = "3.5"
-
-class UnsupportedPythonError(Exception):
-    pass
-
-if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError("packagename does not support Python < {}".format(__minimum_python_version__))
-
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from .main import SherpaFitter, SherpaMCMC, Stat, OptMethod, EstMethod, Dataset, ConvertedModel
+from .main import (SherpaFitter, SherpaMCMC,
+                   Stat, OptMethod, EstMethod,
+                   Dataset, ConvertedModel)
